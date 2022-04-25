2375d0190862ce1b547650514141e04d813b4cb0
chayashivalingegowda <41785270+chayashivalingegowda@users.noreply.github.com>
2022-04-26 05:04:48 +0530

    Update projectbackend.py

--- projectbackend.py
+++ projectbackend.py
@@ -6,22 +6,22 @@ import datetime
 currentDateTime = datetime.datetime.now()
 currentDate = currentDateTime.date()
 def studentdata():
-    con=sqlite3.connect("MYnewDatabase.db")
+    con=sqlite3.connect("GemData.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY, StdID text, Fname text, Lname text, Dob text, Own Car, Timestamp TIMESTAMP, Date TIMESTAMP )")
+    cur.execute("CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY, StdID text, Fname text, Lname text, Timestamp TIMESTAMP, Date TIMESTAMP )")
     con.commit()
     con.close()
 
-def addStdRec(StdID, Fname , Lname , Dob , Owncar):
+def addStdRec(StdID, Fname , Lname):
     studentdata()
-    con = sqlite3.connect("MYnewDatabase.db")
+    con = sqlite3.connect("GemData.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO student VALUES (NULL,?,?,?,?,?,?,?)", (StdID, Fname , Lname , Dob, Owncar, currentDateTime, currentDate))
+    cur.execute("INSERT INTO student VALUES (NULL,?,?,?,?,?)", (StdID, Fname , Lname , currentDateTime, currentDate))
     con.commit()
     con.close()
 
 def Viewdata():
-    con = sqlite3.connect("MYnewDatabase.db")
+    con = sqlite3.connect("GemData.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM student")
     row=cur.fetchall()
@@ -29,17 +29,17 @@ def Viewdata():
     return row
 
 def deletRec(id):
-    con = sqlite3.connect("MYnewDatabase.db")
+    con = sqlite3.connect("GemData.db")
     cur = con.cursor()
     cur.execute("DELETE FROM student WHERE id=?",(id,))
     con.commit()
     con.close()
 
-def searchdata(StdID="", Fname="" , Lname="" , Dob="" , Owncar = ""):
-    con = sqlite3.connect("MYnewDatabase.db")
+def searchdata(StdID="", Fname="" , Lname=""):
+    con = sqlite3.connect("GemData.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM student WHERE StdID=? OR Fname=? OR Lname=? OR Dob=?",\
-                (StdID, Fname , Lname , Dob, Owncar ))
+    cur.execute("SELECT * FROM student WHERE StdID=? OR Fname=? OR Lname=?",\
+                (StdID, Fname , Lname ))
     row = cur.fetchall()
     con.close()
     return row
